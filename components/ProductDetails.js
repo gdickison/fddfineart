c85e1ce03b527d8b33b3b24d82b098dc702257be
paunchymonk <gregory@dickison.net>
2022-09-27 11:47:31 -0700

    Add a better option for the product details component under the current option

--- components/ProductDetails.js
+++ components/ProductDetails.js
@@ -40,6 +40,11 @@ const ProductDetails = ({productDetails}) => {
     }
   }
 
+  const addToCart = e => {
+    e.preventDefault()
+    alert("Added to cart")
+  }
+
   {console.log('productDetails', productDetails)}
   const { title, images, product_variants } = productDetails
   return (
@@ -116,50 +121,68 @@ const ProductDetails = ({productDetails}) => {
           </span>
         </div>
         <div>
-          <button type="button" className="px-8 py-3 font-semibold rounded bg-gray-100 text-gray-800">Add to Cart</button>
+          <button type="button" className="px-8 py-3 font-semibold rounded bg-gray-100 text-gray-800" onClick={addToCart}>Add to Cart</button>
         </div>
       </div>
+      <section className="text-gray-600 body-font overflow-hidden">
+        <div className="container px-5 py-24 mx-auto">
+          <div className="lg:w-4/5 mx-auto flex flex-wrap">
+            <div className="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
+              <h1 className="text-gray-100 text-3xl title-font font-thin mb-4">{title}</h1>
+              <p className="leading-relaxed mb-4 text-xl font-thin text-gray-100">{productDetails.description}</p>
+              <div className="flex justify-between border-t border-gray-200 py-2">
+                <label className="font-thin text-xl mx-2 text-gray-100"  htmlFor="options">Select a Size</label>
+                <select className="w-1/2 mx-2 text-center border-2 border-blue-400 bg-white text-black rounded-md font-thin text-xl" id="size" name="size" defaultValue="" onChange={handleSizeChange}>
+                  <option key="size_24x36" value="size24x36" selected>24x36</option>
+                  <option key="size_8x10" value="size8x10">8x10</option>
+                  <option key="size_36x48" value="size36x48">36x48</option>
+                </select>
+              </div>
+              <div className="flex justify-between border-t border-gray-200 py-2">
+                <label className="font-thin text-xl mx-2 text-gray-100"  htmlFor="options">Select a Media</label>
+                <select className="w-1/2 mx-2 text-center border-2 border-blue-400 bg-white text-black rounded-md font-thin text-xl" id="media" name="media" defaultValue="" onChange={handleMediaChange}>
+                  <option key="media_art_paper" value="media_art_paper" selected>Art Paper</option>
+                  <option key="media_canvas" value="media_canvas">Canvas</option>
+                  <option key="media_saw_blade" value="media_saw_blade">Saw Blade</option>
+                </select>
+              </div>
+              <div className="flex justify-between border-t border-gray-200 py-2">
+              <label className="font-thin text-xl mx-2 text-gray-100"  htmlFor="options">Select a Frame</label>
+              <select className="w-1/2 mx-2 text-center border-2 border-blue-400 bg-white text-black rounded-md font-thin text-xl" id="frame" name="frame" defaultValue="" onChange={handleFrameChange}>
+                <option key="frame_none" value="frame_none" selected>None</option>
+                <option key="frame_wood" value="frame_wood">Wood</option>
+                <option key="frame_other" value="frame_other">Other</option>
+              </select>
+              </div>
+              <div className="flex items-center justify-between border-t border-b mb-6 border-gray-200 py-2">
+                <span className="w-1/2 font-thin text-xl mx-2 text-gray-100">Quantity</span>
+                <div className="w-1/2 flex justify-center">
+                  <span className="flex items-center justify-center w-12 h-12 group hover:cursor-pointer" onClick={subtractQuantity}>
+                    <svg className="fill-current text-gray-100 w-3 group-active:scale-150" viewBox="0 0 448 512">
+                      <path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+                    </svg>
+                  </span>
+                  <span className="flex justify-center items-center bg-white mx-2 border w-12 h-12 text-gray-800">{quantity}</span>
+                  <span className="flex items-center justify-center w-12 h-12 group hover:cursor-pointer" onClick={addQuantity}>
+                    <svg className="fill-current text-gray-100 w-3 group-active:scale-150" viewBox="0 0 448 512">
+                      <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z"/>
+                    </svg>
+                  </span>
+                </div>
+              </div>
+              <div className="flex justify-between items-center">
+                <span className="w-1/2 title-font font-medium text-2xl text-gray-900">$58.00</span>
+                <div className="w-1/2">
+                  <button type="button" className="w-full px-8 py-3 font-semibold rounded bg-gray-100 text-gray-800" onClick={addToCart}>Add to Cart</button>
+                </div>
+              </div>
+            </div>
+            <img className="w-1/2 m-auto" src={images[0]} alt={title} />
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
 
 export default ProductDetails
-
-{/* <section class="text-gray-600 body-font overflow-hidden">
-  <div class="container px-5 py-24 mx-auto">
-    <div class="lg:w-4/5 mx-auto flex flex-wrap">
-      <div class="lg:w-1/2 w-full lg:pr-10 lg:py-6 mb-6 lg:mb-0">
-        <h2 class="text-sm title-font text-gray-500 tracking-widest">BRAND NAME</h2>
-        <h1 class="text-gray-900 text-3xl title-font font-medium mb-4">Animated Night Hill Illustrations</h1>
-        <div class="flex mb-4">
-          <a class="flex-grow text-indigo-500 border-b-2 border-indigo-500 py-2 text-lg px-1">Description</a>
-          <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Reviews</a>
-          <a class="flex-grow border-b-2 border-gray-300 py-2 text-lg px-1">Details</a>
-        </div>
-        <p class="leading-relaxed mb-4">Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia microdosing tilde DIY. XOXO fam inxigo juiceramps cornhole raw denim forage brooklyn. Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over, neutra jean.</p>
-        <div class="flex border-t border-gray-200 py-2">
-          <span class="text-gray-500">Color</span>
-          <span class="ml-auto text-gray-900">Blue</span>
-        </div>
-        <div class="flex border-t border-gray-200 py-2">
-          <span class="text-gray-500">Size</span>
-          <span class="ml-auto text-gray-900">Medium</span>
-        </div>
-        <div class="flex border-t border-b mb-6 border-gray-200 py-2">
-          <span class="text-gray-500">Quantity</span>
-          <span class="ml-auto text-gray-900">4</span>
-        </div>
-        <div class="flex">
-          <span class="title-font font-medium text-2xl text-gray-900">$58.00</span>
-          <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button>
-          <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-            <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-              <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-            </svg>
-          </button>
-        </div>
-      </div>
-      <img alt="ecommerce" class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded" src="https://dummyimage.com/400x400">
-    </div>
-  </div>
-</section> */}
