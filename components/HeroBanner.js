723d109af9b18e8b8f052aa5c4d4998263915cc2
paunchymonk <gregory@dickison.net>
2022-09-28 16:06:09 -0700

    Use wordmark jpg for header

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link"
 import NavBar from "./NavBar"
 
@@ -8,7 +9,8 @@ const HeroBanner = () => {
         <div className="overflow-visible w-full py-0 relative">
           <div className="mb-4 pl-4">
           <Link href="/">
-            <a className="text-4xl font-libre uppercase text-black" style={{'letter-spacing': '0.2em'}}>Forrest Dickison Fine Art</a>
+            {/* <a className="text-4xl font-libre uppercase text-black" style={{'letter-spacing': '0.2em'}}>Forrest Dickison Fine Art</a> */}
+            <img src="/ForrestDickisonWordmark.jpg" alt="Forrest Dickison Fine Art" />
           </Link>
           </div>
           <NavBar/>
