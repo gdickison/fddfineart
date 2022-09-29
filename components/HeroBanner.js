7e3b6550847a2524d91df02ebb53be6020515f0b
paunchymonk <gregory@dickison.net>
2022-09-28 19:54:17 -0700

    Make header smaller

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -7,10 +7,10 @@ const HeroBanner = () => {
     <div className="text-center w-full py-12">
       <div className="max-w-[1170px] my-0 mx-auto relative">
         <div className="overflow-visible w-full py-0 relative">
-          <div className="mb-4 pl-4 hover:cursor-pointer">
+          <div className="mb-4 pl-4 hover:cursor-pointer flex justify-center">
           <Link href="/">
             {/* <a className="text-4xl font-libre uppercase text-black" style={{'letter-spacing': '0.2em'}}>Forrest Dickison Fine Art</a> */}
-            <img src="/ForrestDickisonWordmark.jpg" alt="Forrest Dickison Fine Art" />
+            <img src="/ForrestDickisonWordmark.jpg" alt="Forrest Dickison Fine Art" className="w-[50rem]" />
           </Link>
           </div>
           <NavBar/>
