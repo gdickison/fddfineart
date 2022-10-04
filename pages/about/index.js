a37b6fd35886be49a6b86c4aa14f9b4cab3c87fe
paunchymonk <gregory@dickison.net>
2022-10-04 12:58:28 -0700

    Refactor about page, pt 1 - set image on left, text on right

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,13 +3,11 @@ import { client } from '../../lib/client'
 
 const About = ({bio}) => {
   return (
-    <div className="flex justify-center">
-      <div className="flex flex-col max-w-3xl p-6 gap-4">
-        <img src={bio.imageUrl} alt="" className="flex-shrink-0 object-cover rounded-sm bg-gray-500 aspect-square" />
-        <div className="space-y-4">
-          <h2 className="text-xl font-semibold">{bio.name}</h2>
-          <p>{bio.text}</p>
-        </div>
+    <div className="flex p-6 gap-6 max-w-[1170px] max-h-[70vh]">
+      <img src={bio.imageUrl} alt="" className="flex-shrink-0 object-cover rounded-sm bg-gray-500 aspect-square w-1/2" />
+      <div className={`flex flex-col justify-center ${bio.name === null ? "space-y-0" : "space-y-4"}`}>
+        <h2 className={`text-xl font-semibold ${bio.name === null ? "hidden" : "block"}`}>{bio.name}</h2>
+        <p className='leading-[1.7rem] font-light'>{bio.text}</p>
       </div>
     </div>
   )
