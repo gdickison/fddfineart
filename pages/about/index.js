e824dfbd9ab41cb4981c1e62b10bbf26435863f9
paunchymonk <gregory@dickison.net>
2022-10-04 13:19:52 -0700

    Make sure about page is responsive

--- pages/about/index.js
+++ pages/about/index.js
@@ -3,11 +3,11 @@ import { client } from '../../lib/client'
 
 const About = ({bio}) => {
   return (
-    <div className="flex p-6 gap-6 max-w-[1170px] max-h-[70vh]">
-      <img src={bio.imageUrl} alt="" className="flex-shrink-0 object-cover rounded-sm bg-gray-500 aspect-square w-1/2" />
+    <div className="flex flex-col items-center landscape:flex-row p-6 gap-6 max-w-[1170px] md:max-h-[70vh]">
+      <img src={bio.imageUrl} alt="" className="flex-shrink-0 object-cover rounded-sm bg-gray-500 aspect-square w-full md:w-1/2" />
       <div className={`flex flex-col justify-center ${bio.name === null ? "space-y-0" : "space-y-4"}`}>
         <h2 className={`text-xl font-semibold ${bio.name === null ? "hidden" : "block"}`}>{bio.name}</h2>
-        <p className='leading-[1.7rem] font-light'>{bio.text}</p>
+        <p className='md:leading-[1.7rem] font-light text-2xl md:text-lg'>{bio.text}</p>
       </div>
     </div>
   )
