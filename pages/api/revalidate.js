1ee03fcc196d42b42c2e7744e41d1849c29daa37
paunchymonk <gregory@dickison.net>
2022-10-22 16:16:55 -0700

    one more try

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,22 +1,19 @@
 import {isValidSignature, SIGNATURE_HEADER_NAME} from '@sanity/webhook'
 
 export default async function handler(req, res) {
-console.log('req.body', req.body)
   try {
+    console.log('req.body', req.body)
+    const body = readBody(req)
     const signature = request.headers[SIGNATURE_HEADER_NAME]
-    if(!isValidSignature(
-        JSON.stringify(req.body),
-        signature,
-        process.env.SECRET_REVALIDATE_TOKEN
-      )
-    ){
+    const secret = process.env.SECRET_REVALIDATE_TOKEN
+    if(!isValidSignature(body, signature, secret)){
       return res.status(401).json({ message: 'Invalid request' })
     }
-
-    await res.revalidate(`/product-details/${req.body.slug}`)
-    console.log('slug', req.body.slug)
+    const {slug} = req.body
+    await res.revalidate(`/product-details/${slug}`)
+    console.log('slug', slug)
     res.status(200).json({ message: 'Painting details revalidated' })
   } catch (err) {
-    return res.status(500).send(req.body)
+    return res.status(500).send('Error revalidating')
   }
 }
