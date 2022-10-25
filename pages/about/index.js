846be52098efb1b8e91ff6d18989ef90c95be424
paunchymonk <gregory@dickison.net>
2022-10-24 18:59:09 -0700

    Use Swiper js instead of nuka-carousel on the bio page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,70 +1,62 @@
 /* eslint-disable @next/next/no-img-element */
 import { client } from '../../lib/client'
-import Carousel from 'nuka-carousel/lib/carousel'
-
+// import Swiper JS
+import { Autoplay, Lazy, Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react'
+  
 const About = ({bio}) => {
   const slides = bio.bioImages.map((image, idx) => {
     return (
-      <img
-        key={idx}
-        className="flex-shrink-0 object-cover aspect-square"
-        src={image}
-        alt={`Slide ${idx + 1}`}
-        data-slide={`Slide ${idx + 1}`}
-        style={{
-          width: '100%',
-          paddingLeft: '5px',
-          paddingRight: '5px'
-        }}
-      />
+      <>
+        <img
+          key={idx}
+          className="flex-shrink-0 object-cover aspect-square px-1 w-full swiper-lazy"
+          data-src={image}
+          alt={`Slide ${idx + 1}`}
+          data-slide={`Slide ${idx + 1}`}
+        />
+        <div className="swiper-lazy-preloader"></div>
+      </>
     )
   })
 
   return (
-    <main className="flex flex-col items-center landscape:flex-row  gap-8 landscape:gap-4">
-      {bio.bioImages.length > 1
-        ? <div className="flex items-center justify-center sm:w-2/3 md:w-1/2 h-auto overflow-hidden">
-            <Carousel
-              defaultControlsConfig={{
-                nextButtonText: " ",
-                nextButtonStyle: {
-                  display: 'none',
-                  backgroundImage: 'url("/next_chev.svg")',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundColor: 'transparent',
-                  backgroundPosition: 'left',
-                  height: '2rem',
-                  width: '2.5rem',
-                  padding: 0
-                },
-                pagingDotsStyle: {
-                  padding: '1rem',
-                  fill: 'white'
-                },
-                prevButtonText: " ",
-                prevButtonStyle: {
-                  display: 'none',
-                  backgroundImage: 'url("/previous_chev.svg")',
-                  backgroundRepeat: 'no-repeat',
-                  backgroundColor: 'transparent',
-                  backgroundPosition: 'right',
-                  height: '2rem',
-                  width: '2.5rem',
-                  padding: 0
-                }
-              }}
-              dragThreshold={0}
-              enableKeyboardControls={true}
-              wrapAround={true}
-              autoplay={true}
-              autoplayInterval={8000}
-              cellSpacing={20}
-            >
-              {slides}
-            </Carousel>
-          </div>
-        : <img src={bio.bioImages[0]} alt="" className="flex-shrink-0 object-cover aspect-square w-full md:w-1/2" />
-      }
+    <main className="flex flex-col items-center landscape:flex-row  gap-8">
+      <div className='flex items-center justify-center sm:w-2/3 md:w-1/2 h-auto overflow-hidden'>
+        <Swiper
+          id="swiper-bio-custom"
+          modules={[
+            Autoplay,
+            Lazy,
+            Navigation,
+            Pagination
+          ]}
+          autoplay={{
+            delay: 5000
+          }}
+          lazy={{
+            enabled: true,
+            loadOnTransitionStart: true,
+            loadingClass: 'swiper-lazy-loading'
+          }}
+          loop={true}
+          navigation
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          pagination={{ clickable: true }}
+          slidesPerView={1}
+          spaceBetween={50}
+        >
+          {slides.map((slide, idx) => {
+            return (
+              <SwiperSlide key={idx}>
+                {slide}
+                <div className="swiper-lazy-preloader"></div>
+              </SwiperSlide>
+            )
+          })}
+        </Swiper>
+      </div>
       <div className={`flex flex-col justify-center ${bio.name === null ? "space-y-0" : "space-y-4"} sm:w-2/3 md:w-1/2 landscape:pr-6 text-justify`}>
         <h2 className={`text-xl font-semibold ${bio.name === null ? "hidden" : "block"}`}>{bio.name}</h2>
         <p className='font-thin md:leading-[1.7rem] md:text-lg p-4 md:p-0'>{bio.text}</p>
