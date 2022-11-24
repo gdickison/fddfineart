cd4ebaf19f8ae38da959e1e6bf1c4c2fb6ed2658
paunchymonk <gregory@dickison.net>
2022-11-23 18:36:10 -0800

    More fly memory just made it slower

--- components/PaintingsInCollection.js
+++ components/PaintingsInCollection.js
@@ -2,7 +2,7 @@
 import { useState } from "react"
 import ProductModal from "./ProductModal"
 import { urlFor } from "../lib/client"
-import Image from "next/image"
+// import Image from "next/image"
 
 export default function PaintingsInCollection ({paintings}) {
   const [showModal, setShowModal] = useState(false)
@@ -28,7 +28,7 @@ export default function PaintingsInCollection ({paintings}) {
             className="flex justify-center hover-effect hover:cursor-pointer"
             onClick={!painting.placeholder ? e => openModal(e, idx, paintings) : e => e.preventDefault()}
           >
-            <Image
+            <img
               src={urlFor(painting.imageUrl).auto('format').url()}
               width={370}
               height={300}
