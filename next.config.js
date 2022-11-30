9fc4323ba5fe80cf06553547f15ba6a035dedb43
paunchymonk <gregory@dickison.net>
2022-11-29 17:53:40 -0800

    Add forrestdickison.com to the domains array

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['cdn.sanity.io']
+    domains: ['forrestdickison.com', 'cdn.sanity.io']
   }
   // images: {
   //   remotePatterns: [
